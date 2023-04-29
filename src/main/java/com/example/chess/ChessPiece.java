b326705007c004806305399ca51c3714bb98e6f5
ale99viv <alessio99vivaldelli@gmail.com>
2023-04-29 12:28:03 +0200

    Drag and drop optimized

--- src/main/java/com/example/chess/ChessPiece.java
+++ src/main/java/com/example/chess/ChessPiece.java
@@ -19,7 +19,12 @@ public class ChessPiece extends ImageView  {
     private final String type;
     private final String color;
     private static GridPane gridPane;
-    private static List<Integer> highlighted = new ArrayList<>();
+    private static int prevHigh = -1;
+    private static boolean isPressed = false;
+    private static int currentX = -1; private static int currentY = -1;
+    private static int fromX = -1; private static int fromY = -1;
+
+    private static boolean isValidIndex = false;
 
     private final boolean hasMoved;
     public ChessPiece(String type, String color, boolean hasMoved) {
@@ -39,85 +44,78 @@ public class ChessPiece extends ImageView  {
         setFitHeight(cell);
         setImage(image);
 
-        setOnDragDetected(event -> {
-            Dragboard db = startDragAndDrop(TransferMode.ANY);
-
-            ClipboardContent content = new ClipboardContent();
-            content.putString("drop");
-            db.setContent(content);
-
-
+        setOnMousePressed(event -> {
             StackPane parent = (StackPane) this.getParent();
-            int colS = (GridPane.getColumnIndex(parent));
-            int rowS = (GridPane.getRowIndex(parent));
+            fromX = (GridPane.getColumnIndex(parent));
+            fromY = (GridPane.getRowIndex(parent));
 
             gridPane = (GridPane) parent.getParent();
 
             List<Integer[]> em = new ArrayList<>();
             Integer[] em_1 = {-1,-1};
-            ChessClick.setClick(rowS, colS, parent, em, em_1);
+            ChessClick.setClick(fromY, fromX, parent, em, em_1);
 
-            startFullDrag();
-            event.consume();
+            if (this.getParent() != ChessBoard.pane) {
+                double colS_2 = ((GridPane.getColumnIndex(parent)) * ChessBoard.CELL_SIZE) + ((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2);
+                double rowS_2 = ((GridPane.getRowIndex(parent)) * ChessBoard.CELL_SIZE) + ((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2);
+
+                parent.getChildren().remove(0);
+                Pane mainPane = ChessBoard.pane;
+                mainPane.getChildren().add(this);
+
+                setX(colS_2);
+                setY(rowS_2);
+            }
+            isPressed = true;
         });
-        setOnDragDropped(event -> {
-            Dragboard db = event.getDragboard();
-            boolean success = db.hasString();
 
-            event.consume();
-
-        });
-        this.setOnDragOver(event -> {
-
-            if (event.getGestureSource() == this && event.getDragboard().hasString()) {
-                if (this.getParent() != ChessBoard.pane) {
-                    StackPane parent = (StackPane) this.getParent();
-                    double colS = ((GridPane.getColumnIndex(parent)) * ChessBoard.CELL_SIZE) + ((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2);
-                    double rowS = ((GridPane.getRowIndex(parent)) * ChessBoard.CELL_SIZE) + ((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2);
-
-                    parent.getChildren().remove(0);
-                    Pane mainPane = ChessBoard.pane;
-                    mainPane.getChildren().add(this);
-
-                    setX(rowS);
-                    setY(colS);
-                }
+        setOnMouseDragged(event -> {
                 double mouseX = event.getSceneX();
                 double mouseY = event.getSceneY();
 
                 setX(mouseX - getFitWidth()/2);
                 setY(mouseY - getFitHeight()/2);
 
-                int posX = (int) Math.floor((Math.min((Math.max((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2, getX())),
+                currentX = (int) Math.floor((Math.min((Math.max((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2, getX())),
                         ChessBoard.BOARD_SIZE + (ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2)) / ChessBoard.CELL_SIZE) - 1;
 
-                int posY = (int) Math.floor((Math.min((Math.max((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2, getY())),
+                currentY = (int) Math.floor((Math.min((Math.max((ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2, getY())),
                         ChessBoard.BOARD_SIZE + (ChessBoard.WINDOW_SIZE - ChessBoard.BOARD_SIZE)/2)) / ChessBoard.CELL_SIZE) - 1;
 
-                StackPane stack = (StackPane) gridPane.getChildren().get(posY * 8 + posX+1);
+                StackPane stack = (StackPane) gridPane.getChildren().get(currentY * 8 + currentX+1);
 
-                List<Integer> copy = highlighted;
-                int count = 0;
-                for (int index : copy) {
-                    if (index != posY * 8 + posX + 1) {
-                        StackPane sta = (StackPane) gridPane.getChildren().get(index);
-                        sta.setStyle("-fx-border-color: transparent;-fx-border-width: 0px;-fx-background-color:" + ChessClick.getSquareColor((index - 1) / 8,  (index - 1) % 8));
-                        highlighted.remove(count);
-                    }
-                    count += 1;
+                if (prevHigh != currentY * 8 + currentX+1 && prevHigh != -1){
+                    StackPane sta = (StackPane) gridPane.getChildren().get(prevHigh);
+                    sta.setStyle("-fx-border-color: transparent;-fx-border-width: 0px;-fx-background-color:" + ChessClick.getSquareColor((prevHigh - 1) / 8,  (prevHigh - 1) % 8));
                 }
 
+                if (stack.getChildren().size() == 0){isValidIndex = false;}
                 for (Node e : stack.getChildren()){
                     if (e instanceof VBox){
                         stack.setStyle("-fx-border-color: transparent; -fx-border-width: 0.0; -fx-background-color: rgba(0, 0, 255, 0.5);");
                         stack.setId("drag");
-                        highlighted.add(posY * 8 + posX+1);
+                        prevHigh = currentY * 8 + currentX+1;
+                        isValidIndex = true;
                     }
                 }
+        });
 
-                event.acceptTransferModes(TransferMode.ANY);
+        setOnMouseReleased(event -> {
+            if (isPressed)
+            {
+                if (currentX != -1 && currentY != -1){
+                    if (isValidIndex) {
+                        StackPane toPane = (StackPane) gridPane.getChildren().get(currentY * 8 + currentX + 1);
+                        toPane.getChildren().add(this);
+                    }
+                    else {
+                        StackPane toPane = (StackPane) gridPane.getChildren().get(fromY * 8 + fromX + 1);
+                        toPane.getChildren().add(this);
+                    }
+                }
+                prevHigh = -1;
+                isValidIndex = false;
             }
-            event.consume();
         });
     }
     public String getColor() {
